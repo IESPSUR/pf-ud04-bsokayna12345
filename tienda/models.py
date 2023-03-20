8f2e3778987da3439c148e0b2c949861da3e6fea
bsokayna12345 <sokayna.bagari.al@iespoligonosur.org>
2023-03-20 10:53:26 +0100

    models nuevos

--- tienda/models.py
+++ tienda/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Marca(models.Model):
+    nombre=models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.nombre
+class Producto(models.Model)
+    marca=models.ForeignKey('Marca', on_delete=models.CASCADE)
+    nombre=models.CharField(max_length=30)
+
