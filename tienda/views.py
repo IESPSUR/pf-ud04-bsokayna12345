09509f9f02acc74e205d6020ba0337af40a8383c
bsokayna12345 <sokayna.bagari.al@iespoligonosur.org>
2023-03-21 00:48:18 +0100

    list product

--- tienda/views.py
+++ tienda/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 
+from tienda.models import Producto
+
+
 # Create your views here.
 def welcome(request):
-    return render(request,'tienda/index.html', {})
+    return render(request, 'tienda/index.html', {})
+
+
+def listProducto(request):
+    context = {}
+    productos = Producto.objects.all()
+    context["datos"] = productos
+    return render(request, "listProducto.html", context)
