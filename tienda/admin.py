d4804bd9dd25739dfe1a4f4b2bec83c937959d17
bsokayna12345 <sokayna.bagari.al@iespoligonosur.org>
2023-03-20 10:56:36 +0100

    Revert "modelos, admin"
    This reverts commit c7a84531afffbe75e6ba97a5915369f508094567.

--- tienda/admin.py
+++ tienda/admin.py
@@ -1,7 +1,3 @@
 from django.contrib import admin
-#importar los models que queremos administrar 
-from tienda.models import Producto, Marca
 
-# Register your models here. 
-admin.site.register(Producto)
-admin.site.register(Marca)
+# Register your models here.
--- tienda/models.py
+++ tienda/models.py
@@ -1,26 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class Marca(models.Model):
-    nombre_marca = models.CharField(max_length=50, primary_key=True)
-    #
-    def __str__(self):
-        return self.nombre_marca
-
-class Producto(models.Model):
-    marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
-    nombre = models.CharField(max_length=20, primary_key=True)
-    modelo = models.IntegerField()
-    unidades = models.IntegerField()
-    precio = models.IntegerField()
-    detalles = models.CharField(max_length=100, blank=True)
-
-    def __str__(self):
-        return '{} {}'.format(self.nombre, self.marca)
-
-class Compra(models.Model):
-    nombre_producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
-    #user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
-    importe = models.FloatField()
-    unidades = models.IntegerField()
-    fecha = models.DateField()
