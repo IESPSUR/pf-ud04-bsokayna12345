322d79af65b006895de6da3984029127421a7bf8
bsokayna12345 <sokayna.bagari.al@iespoligonosur.org>
2023-04-12 01:42:43 +0200

    modificacion

--- tienda/forms.py
+++ tienda/forms.py
@@ -1,14 +1,12 @@
 import itertools
-
 from django.contrib.auth.models import User
 from django.forms import ModelForm
 from django import forms
-
 from tienda.models import Producto, Compra, Marca
 
 
 class FormProducto(ModelForm):
-    unidades=forms.IntegerField(min_value=0)
+    unidades=forms.IntegerField(min_value=1)
     class Meta:
         model = Producto
         fields = '__all__'
@@ -16,11 +14,9 @@ class FormProducto(ModelForm):
 
 class FormCompra(ModelForm):
     nombre = forms.CharField(required=False)
-
     class Meta:
         model = Producto
         fields = ['nombre']
-
 
 class FormCheckout(ModelForm):
     unidades = forms.IntegerField(widget=forms.HiddenInput)
@@ -28,13 +24,10 @@ class FormCheckout(ModelForm):
         model = Producto
         fields = ['unidades']
 
-
 class FormMarca(forms.Form):
     choices = Marca.objects.all().values_list('nombre', 'nombre')
     empty = [('', '-------')]
     marca = forms.ChoiceField(required=False, choices=itertools.chain(empty, choices.iterator()))
-
-
 
 class FormCompraUser(forms.Form):
     users = forms.ModelChoiceField(queryset=User.objects.all(), label='Seleccione un usuario')
